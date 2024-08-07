0436442f3281ecf5d49de7e70d8e557eb4563bf0
Frankwillians <fkffrannk02@gmail.com>
2024-08-07 13:07:42 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,18 @@
 package com.educandoweb.course.entities;
 
+
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) /*serve para incrementar o numero do id automaticamente*/
     private Long id;
     private String name;
     private String email;
